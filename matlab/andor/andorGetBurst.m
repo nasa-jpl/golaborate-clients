f571f7e5cc77180fa7185a20e9d9dfb27a2709d3
Carl Coker <carl.t.coker@jpl.nasa.gov>
2020-02-07 16:32:43 -0800

    Added docstring to andorGetBurst.m

--- matlab/andor/andorGetBurst.m
+++ matlab/andor/andorGetBurst.m
@@ -1,4 +1,7 @@
 function ary = andorGetBurst(s, frames, fps)
+% img_array = andorGetBurst(camera struct, number of frames, frames per second).
+% Take a burst of images from an Andor camera.  The images are saved into an array
+% arranged so that the 3rd dimension walks through the images.
 
     writeopts = weboptions('Timeout', Inf, 'MediaType', 'application/json');
     opts = weboptions('Timeout', Inf);
