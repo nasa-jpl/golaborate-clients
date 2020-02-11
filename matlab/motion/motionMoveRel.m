f825bfe742c8b60d2255eb1cee72330513547341
Brandon Dube <brandon.dube@jpl.nasa.gov>
2020-02-11 12:56:06 -0800

    update matlab timeouts

--- matlab/motion/motionMoveRel.m
+++ matlab/motion/motionMoveRel.m
@@ -1,6 +1,6 @@
 function motionMoveRel(s, delta)
     url = strcat(s.ControllerURL, "/axis/", s.Axis, "/pos?relative=true");
-    opts = weboptions('MediaType', 'application/json');
+    opts = weboptions('MediaType', 'application/json', 'timeout', Inf);
     payload = struct();
     payload.f64 = delta;
     webwrite(url, payload, opts);
