19b97e3e40aa8ef7e90d1edd50675cf76176b724
Brandon Dube <brandon.dube@jpl.nasa.gov>
2020-04-02 13:51:21 -0700

    rework python interface, uses raise_err like other gohcit clients and f-strings
    fix some URL changes made recently on the server too.

--- nkt/__init__.py
+++ nkt/__init__.py
@@ -2,20 +2,44 @@
 
 import requests
 
+def raise_err(resp):
+    """Raise an exception if the response status code is not 200.
+
+    Parameters
+    ----------
+    resp : `requests.Response`
+        a response with a status code
+
+    Raises
+    ------
+    Exception
+    any errors encountered, whether they are in communciation with the
+    server or between the server and the camera/SDK
+
+    """
+    if resp.status_code != 200:
+        raise Exception(resp.content.decode('UTF-8').rstrip())
+
 
 class SuperK:
     """SuperK enables programmatic control of NKT SuperK lasers."""
 
-    def __init__(self, ip):
+    def __init__(self, addr):
         """Create a new SuperK instance.
 
         Parameters
         ----------
-        ip : `str`
+        addr : `str`
             IP address (port included) that the HTTP adapter server is listening at
 
         """
-        self.ip = "http://" + ip
+        if not addr.startswith('http://'):
+            addr = 'http://' + addr
+
+        if 'https' in addr:
+            addr = addr.replace('https', 'http')
+
+        self.addr = addr
 
     def center_bandwidth(self, center=None, bw=None):
         """Get or set the center wavelength and full bandwidth.
@@ -39,27 +63,71 @@ class SuperK:
         or raises an error on failure.
 
         """
-        url = self.ip + "/wl-center-bandwidth"
+        url = f'{self.addr}/wvl/center-bandwidth'
         if center is None:
             resp = requests.get(url)
-            if resp.status_code != 200:
-                raise Exception(resp.content)
+            raise_err(resp)
             json = resp.json()
             return json['center'], json['bandwidth']
         else:
             data = {'center': center, 'bandwidth': bw}
             resp = requests.post(url, json=data)
-            if resp.status_code != 200:
-                raise Exception(resp.content)
+            raise_err(resp)
+
+    def short_wave(self, wvl_nm=None):
+        """Get or set the short wavelength of the VARIA.
+
+        Parameters
+        ----------
+        wvl_nm : `float`
+            wavelength in nanometers, if None gets, else sets
+
+        Returns
+        -------
+        `float`
+            short wavelength, nm
+
+        """
+        url = f'{self.addr}/wvl/short'
+        if wvl_nm is None:
+            resp = requests.get(url)
+            raise_err(resp)
+            return resp.json()['f64']
+        else:
+            payload = {'f64': float(wvl_nm)}
+            resp = requests.post(url, json=payload)
+            raise_err(resp)
+
+    def long_wave(self, wvl_nm=None):
+        """Get or set the long wavelength of the VARIA.
+
+        Parameters
+        ----------
+        wvl_nm : `float`
+            wavelength in nanometers, if None gets, else sets
+
+        Returns
+        -------
+        `float`
+            short wavelength, nm
+
+        """
+        url = f'{self.addr}/wvl/long'
+        if wvl_nm is None:
+            resp = requests.get(url)
+            raise_err(resp)
+            return resp.json()['f64']
+        else:
+            payload = {'f64': float(wvl_nm)}
+            resp = requests.post(url, json=payload)
+            raise_err(resp)
 
     def emission(self, on=None):
         """Get or set the emission.
 
         Notes
         -----
-        If on is None, gets the state, returns bool.
-        If on is True, turns the laser on.  No return, or error.
-        If on is False, turns the laser off.  No return, or error.
+        If on is None, gets the state, returns bool.  Otherwise manipulates emission state
 
         Parameters
         ----------
@@ -72,20 +140,15 @@ class SuperK:
             laser on/off
 
         """
+        url = f'{self.addr}/emission'
         if on is None:
-            url = self.ip + "/emission"
+            resp = requests.get(url)
+            raise_err(resp)
+            return resp.json()['bool']
         else:
-            if on is False:
-                url = self.ip + "/emission/off"
-            elif on is True:
-                url = self.ip + "/emission/on"
-
-        resp = requests.get(url)
-        if resp.status_code != 200:
-            raise Exception(resp.content)
-        else:
-            if on is None:
-                return resp.json()['bool']
+            payload = {'bool': on}
+            resp = requests.post(url, json=payload)
+            raise_err(resp)
 
     def ND(self, pct=None):
         """Get or set the VARIA ND strength.
@@ -101,18 +164,15 @@ class SuperK:
             ND strength, percent
 
         """
-        url = self.ip + "/nd"
+        url = f'{self.addr}/nd'
         if pct is None:
             resp = requests.get(url)
-            if resp.status_code != 200:
-                raise Exception(resp.content)
-            else:
-                return resp.json()['f64']
+            raise_err(resp)
+            return resp.json()['f64']
         else:
             payload = {'f64': pct}
             resp = requests.post(url, json=payload)
-            if resp.status_code != 200:
-                raise Exception(resp.content)
+            raise_err(resp)
 
     def power(self, pct=None):
         """Get or set the main module power level.
@@ -128,33 +188,26 @@ class SuperK:
             power level, percent
 
         """
-        url = self.ip + "/power"
+        url = f'{self.addr}/power'
         if pct is None:
             resp = requests.get(url)
-            if resp.status_code != 200:
-                raise Exception(resp.content)
-            else:
-                return resp.json()['f64']
+            raise_err(resp)
+            return resp.json()['f64']
         else:
             payload = {'f64': pct}
             resp = requests.post(url, json=payload)
-            if resp.status_code != 200:
-                raise Exception(resp.content)
+            raise_err(resp)
 
     def status_main(self):
         """Get the status bitfield from the main module."""
         url = self.ip + "/main-module-status"
         resp = requests.get(url)
-        if resp.status_code != 200:
-            raise Exception(resp.content)
-        else:
-            return resp.json()
+        raise_err(resp)
+        return resp.json()
 
     def status_varia(self):
         """Get the status bitfield from the VARIA module."""
         url = self.ip + "/varia-status"
         resp = requests.get(url)
-        if resp.status_code != 200:
-            raise Exception(resp.content)
-        else:
-            return resp.json()
+        raise_err(resp)
+        return resp.json()
