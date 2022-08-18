2f7f6c8b766e647e540916cb22ec39f3fd270cf6
Brandon Dube <brandon.dube@jpl.nasa.gov>
2022-08-18 08:51:00 -0700

    common: tweak retry to avoid decorator dependency

--- golab_common/retry.py
+++ golab_common/retry.py
@@ -1,30 +1,30 @@
 """Retry, even simpler-er for windows support."""
 import logging
 import time
-from decorator import decorator
+from functools import wraps
 
 
 def retry(max_retries=2, interval=1):
     # simplified retry decorator, for better windows support
 
     # no use of select from stdlib, other features not used removed
-    @decorator
-    def wrapper(func, *args, **kwargs):
-        result = None
-        last_exception = None
-        for n in range(max_retries):
-            try:
-                result = func(*args, **kwargs)
-                last_exception = None
-                break
-            except Exception as e:
-                last_exception = e
-                logging.info(f'Exception {e} encountered during {func.__name__}, retrying ({n}/max_retries)')
-                time.sleep(interval)
+    def decorator(func):
+        @wraps(func)
+        def wrapper(func, *args, **kwargs):
+            result = None
+            last_exception = None
+            for n in range(max_retries):
+                try:
+                    result = func(*args, **kwargs)
+                    last_exception = None
+                    break
+                except Exception as e:
+                    last_exception = e
+                    logging.info(f'Exception {e} encountered during {func.__name__}, retrying ({n}/max_retries)')
+                    time.sleep(interval)
 
-        if result is None and last_exception is not None:
-            raise last_exception
-        return result
-
-    # end of wrapper, now back in 'retry' scopy
-    return wrapper
+            if result is None and last_exception is not None:
+                raise last_exception
+            return result
+        return wrapper
+    return decorator
